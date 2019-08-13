0a332ce0f8cee242938c44f01d6b4fe101bde54e
LeoFi <eleonorefilhol@gmail.com>
2019-08-13 10:15:47 +0200

    Apy Keys

---
+++ constants/ApiKeys.js
@@ -0,0 +1,11 @@
+export default {
+    FirebaseConfig: {
+        apiKey: "AIzaSyCDxv5wgaNMcA0RWkQ1wKKY9DANdZHQB0I",
+        authDomain: "contenance-48338.firebaseapp.com",
+        databaseURL: "https://contenance-48338.firebaseio.com",
+        projectId: "contenance-48338",
+        storageBucket: "contenance-48338.appspot.com",
+        messagingSenderId: "748933876543",
+        appId: "1:748933876543:web:34fe62990bf37c7d"
+    },
+}
